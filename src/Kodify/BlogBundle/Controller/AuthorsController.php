fcf3f6d0b73bcd39c3ba11bb34bac695e9f71b94
sbenitezma <neithar.aradia@gmail.com>
2016-12-09 01:12:34 +0100

    Remove unnecessary action (already managed by FOSUserBundle) + routings

--- src/Kodify/BlogBundle/Controller/AuthorsController.php
+++ src/Kodify/BlogBundle/Controller/AuthorsController.php
@@ -23,35 +23,4 @@ class AuthorsController extends Controller
 
         return $this->render('KodifyBlogBundle::Author/list.html.twig', array('authors' => $authors));
     }
-
-    /**
-     * Function to create a new author
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function createAction(Request $request)
-    {
-        $form = $this->createForm(
-            new AuthorType(),
-            new Author(),
-            [
-                'action' => $this->generateUrl('create_author'),
-                'method' => 'POST',
-            ]
-        );
-        $parameters = [];
-
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            $author = $form->getData();
-            $this->getDoctrine()->getManager()->persist($author);
-            $this->getDoctrine()->getManager()->flush();
-            $parameters['message'] = 'Author Created!';
-        }
-
-        // the form element should be passed to the view after validate it to show errors
-        $parameters['form'] = $form->createView();
-
-        return $this->render('KodifyBlogBundle:Default:create.html.twig', $parameters);
-    }
 }
